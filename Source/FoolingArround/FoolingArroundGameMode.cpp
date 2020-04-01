d8ebf0c734bcd3a507348580d6c0a03266f9929b
Marko Spasenoski <marko.spasenoski@gmail.com>
2020-04-01 16:26:30 +0200

    init commit

---
+++ Source/FoolingArround/FoolingArroundGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "FoolingArroundGameMode.h"
+#include "FoolingArroundCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AFoolingArroundGameMode::AFoolingArroundGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
